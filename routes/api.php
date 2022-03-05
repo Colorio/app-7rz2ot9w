dab48e6d5070ab8c06fbda5471b36e2fa879bf0c
Gustavo Colorio <tecnologia@soingressos.com>
2022-03-04 21:26:27 -0300

    Start api development / Create ping endpoint

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::namespace('\App\Http\Controllers\Api')->group(function(){
+    Route::get('ping', function(){
+        return response()->json(["status" => "connected", "time" => now()]);
+    });
+});
